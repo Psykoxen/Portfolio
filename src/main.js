687fa8176d5d26bae1bf55b9ad6a1ba84d4c5544
Antoine Voillot <antoine.voillot@etu.univ-lyon1.fr>
2024-01-22 23:01:16 -0500

    🦄 refactor: Cleaning Files

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue"; // Importez createApp depuis Vue 3
-import "./assets/main.css";
-import App from "./App.vue";
-import { i18n } from "./assets/locales/i18n";
-
-const app = createApp(App);
-app.use(i18n);
-app.mount("#app");
