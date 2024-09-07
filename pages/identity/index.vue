129541852b1c767a4f82a8e3b17074f7597b2152
Antoine Voillot <antoine.voillot@etu.univ-lyon1.fr>
2024-09-07 14:55:45 +0200

    feat: Adding identity for QR Luggage card

---
+++ pages/identity/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="h-screen flex flex-col justify-center">
+    <div class="flex flex-col justify-center items-center pt-10">
+      <h1 class="lg:text-9xl text-7xl font-semibold pb-6">Identification</h1>
+      <p class="font-semibold">Antoine VOILLOT</p>
+      <p>385 Rue du Domaine du Moulin, 34380 Saint-Martin-de-Londres</p>
+      <div class="flex gap-4 items-center pt-8">
+        <a href="tel:+33667408248" class="contact-button">Appel</a>
+        <a href="sms:+33667408248" class="contact-button">SMS</a>
+        <a href="mailto:antoine.voillot@yahoo.fr" class="contact-button"
+          >Email</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.contact-button {
+  padding: 0.5em 1em;
+  border-radius: 0.5em;
+  background-color: #191e4e;
+  color: white;
+  text-decoration: none;
+}
+</style>
