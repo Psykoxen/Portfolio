3a4e51e63674230d97c477a094d2f8905f8d691c
Antoine Voillot <antoine.voillot@etu.univ-lyon1.fr>
2024-12-16 16:10:53 +0100

    fix: :bug: Fix size for smartphone

--- pages/identity/index.vue
+++ pages/identity/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="h-screen flex flex-col justify-center">
-    <div class="flex flex-col justify-center items-center pt-10">
-      <h1 class="lg:text-9xl text-7xl font-semibold pb-6">Identification</h1>
+    <div class="flex flex-col justify-center items-center pt-10 pl-2 pr-2">
+      <h1 class="lg:text-9xl sm:text-6xl text-5xl font-semibold pb-6">
+        Identification
+      </h1>
       <p class="font-semibold">Antoine VOILLOT</p>
-      <p>385 Rue du Domaine du Moulin, 34380 Saint-Martin-de-Londres</p>
+      <p class="text-center">
+        385 Rue du Domaine du Moulin, 34380 Saint-Martin-de-Londres
+      </p>
       <div class="flex gap-4 items-center pt-8">
         <a href="tel:+33667408248" class="contact-button">Appel</a>
         <a href="sms:+33667408248" class="contact-button">SMS</a>
